d468a59cb73d4102675b7f91dcb28643e8d99229
BRBYTES <77170070+BRBYTES@users.noreply.github.com>
2021-01-08 13:28:34 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
 radio.setGroup(1)
 radio.setTransmitPower(7)
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft) == true) {
+        basic.showArrow(ArrowNames.West)
+        radio.sendString("left")
+    } else if (input.isGesture(Gesture.TiltRight) == true) {
+        basic.showArrow(ArrowNames.East)
+        radio.sendString("right")
+    } else if (input.isGesture(Gesture.LogoUp) == true) {
+        basic.showArrow(ArrowNames.South)
+        radio.sendString("backward")
+    } else if (input.isGesture(Gesture.LogoDown) == true) {
+        basic.showArrow(ArrowNames.North)
+        radio.sendString("forward")
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
