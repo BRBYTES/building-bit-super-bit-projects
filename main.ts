18ecd233c13386ad9d80769fa5fd3d972b4caf19
BRBYTES <77170070+BRBYTES@users.noreply.github.com>
2021-01-08 14:04:25 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,6 +14,7 @@ basic.forever(function () {
         basic.showArrow(ArrowNames.North)
         radio.sendString("forward")
     } else {
+        radio.sendString("stop")
         basic.showIcon(IconNames.No)
     }
 })
